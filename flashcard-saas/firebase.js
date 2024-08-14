429e2cbe0d378bd006510d9ecc6911eaad04144f
KumariAnjali <kumarianjali206700@gmail.com>
2024-08-15 00:15:12 +0530

    import files

---
+++ flashcard-saas/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSoUMfhI9RrtucvaZSFeQ2FB4Ng0Wb_aY",
+  authDomain: "flashcard-saas-c2bb5.firebaseapp.com",
+  projectId: "flashcard-saas-c2bb5",
+  storageBucket: "flashcard-saas-c2bb5.appspot.com",
+  messagingSenderId: "258722220527",
+  appId: "1:258722220527:web:b0404567b2572028808e1a",
+  measurementId: "G-6M6MMT9P2Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
