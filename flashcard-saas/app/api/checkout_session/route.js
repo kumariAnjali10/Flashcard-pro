6084a00396a519bed39f184d4dec9664c2e538a9
KumariAnjali <kumarianjali206700@gmail.com>
2024-08-17 00:57:27 +0530

    implemented UI

--- flashcard-saas/app/api/checkout_session/route.js
+++ flashcard-saas/app/api/checkout_session/route.js
@@ -4,6 +4,22 @@ const stripe = new Stripe (process.env.STRIPE_SECRET_KEY)
 const formatAmountForStripe = (amount, currency) => {
     return Math.round(amount * 100)
    }
+
+
+   export async function GET(req)
+   {
+    const searchParams = req.nextUrl.searchParams
+    const session_id = searchParams.get('session_id')
+    try{
+      const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+      return NextResponse.json(checkoutSession)
+    }
+    catch(error){
+      console.error('Error retrieving checkout session: ', error)
+       return NextResponse.json({error: {message: error.message}}, {status:500})
+
+    }
+   }
 export async function POST(req){
     
     const params = {
@@ -26,11 +42,11 @@ export async function POST(req){
           },
         ],
         success_url: `${req.headers.get(
-          'Referer',
-        )}result?session_id={CHECKOUT_SESSION_ID}`,
+          'origin',
+        )}/result?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.get(
-          'Referer',
-        )}result?session_id={CHECKOUT_SESSION_ID}`,
+          'origin',
+        )}/result?session_id={CHECKOUT_SESSION_ID}`,
       }
       
       const checkoutSession = await stripe.checkout.sessions.create(params)
--- flashcard-saas/app/api/generate/route.js
+++ flashcard-saas/app/api/generate/route.js
@@ -3,7 +3,19 @@ import OpenAI from "openai";
 
 const systemPrompt = `
 You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 10 flashcards.
-Both front and back should be one sentence long.
+Both front and back should be one sentence long.Follow these guidelines:
+
+1. Create clear and concise questions for the front of the flashcard.
+2. Provide accurate and informative answers for the back of the flashcard.
+3. Ensure that each flashcard focuses on a single concept or piece of information.
+4. Use simple language to make the flashcards accessible to a wide range of learners.
+5. Include a variety of question types, such as definitions, examples, comparisons, and applications.
+6. Avoid overly complex or ambiguous phrasing in both questions and answers.
+7. When appropriate, use mnemonics or memory aids to help reinforce the information.
+8. Tailor the difficulty level of the flashcards to the user's specified preferences.
+9. If given a body of text, extract the most important and relevant information for the flashcards.
+10. Aim to create a balanced set of flashcards that covers the topic comprehensively.
+11. Only generate 10 flashcards.
 
 You should Return in the following JSON format:
 {
@@ -28,9 +40,8 @@ export async function POST(req) {
     model: "gpt-4o",
     response_format: { type: "json_object" },
   });
-
-  // We'll process the API response in the next step
-  // Parse the JSON response from the OpenAI API
+  
+  console.log(completion.choices[0].message.content)
   const flashcards = JSON.parse(completion.choices[0].message.content);
 
   // Return the flashcards as a JSON response
